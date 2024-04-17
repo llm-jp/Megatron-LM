613ac7c6c17de071dcea9f67abeed48b353407e7
kazuki <kaz.tokyo.tech20@gmail.com>
2024-04-17 22:29:58 +0900

    feat: mpirun 対応

--- megatron/training/arguments.py
+++ megatron/training/arguments.py
@@ -60,10 +60,15 @@ def parse_args(extra_args_provider=None, ignore_unknown_args=False):
         assert args.yaml_cfg and args.use_mcore_models, "To use yaml, mcore must be enabled"
         args = load_yaml(args.yaml_cfg)
 
+    # Distributed args.
+    if args.use_mpi:
+        global_rank = int(os.getenv('OMPI_COMM_WORLD_RANK', 0))
+        local_rank = int(os.getenv('OMPI_COMM_WORLD_LOCAL_RANK', 0))
+        world_size = int(os.getenv('OMPI_COMM_WORLD_SIZE', 1))
 
-    # Args from environment
-    args.rank = int(os.getenv('RANK', '0'))
-    args.world_size = int(os.getenv("WORLD_SIZE", '1'))
+        os.environ['RANK'] = str(global_rank)
+        os.environ['LOCAL_RANK'] = str(local_rank)
+        os.environ['WORLD_SIZE'] = str(world_size)
 
     return args
 

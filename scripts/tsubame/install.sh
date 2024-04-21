0ca302850d73d24b4d05390677c9270f3863769a
kazuki <kaz.tokyo.tech20@gmail.com>
2024-04-21 22:41:59 +0900

    env: t4 install script

--- scripts/tsubame/install.sh
+++ scripts/tsubame/install.sh
@@ -11,7 +11,7 @@ module load cuda/12.1.0
 module load nccl/2.20.5
 module load openmpi/5.0.2-gcc
 module load ninja/1.11.1
-module load ~/modulefiles/cudnn/9.0.0
+module load ~/modulefiles/cudnn/8.9.7
 
 # Set environment variables
 source .env/bin/activate
@@ -30,11 +30,13 @@ cd apex
 pip install -v --disable-pip-version-check --no-cache-dir --no-build-isolation --config-settings "--build-option=--cpp_ext" --config-settings "--build-option=--cuda_ext" ./
 
 # transformer engine
-pip install git+https://github.com/NVIDIA/TransformerEngine.git@stable
+pip install git+https://github.com/NVIDIA/TransformerEngine.git@v1.4
 
 # flash-atten
 cd ..
 git clone git@github.com:Dao-AILab/flash-attention.git
 cd flash-attention
 
-python setup.py install
+git checkout v2.4.2
+
+pip install -e .

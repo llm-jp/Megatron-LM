bff8b225c53a87a9cb23869f87ceb4f854968503
kazuki <kaz.tokyo.tech20@gmail.com>
2024-05-02 13:33:23 +0900

    feat: upload script

--- scripts/tsubame/upload/upload_llama3_8b.sh
+++ scripts/tsubame/upload/upload_llama3_8b.sh
@@ -6,12 +6,17 @@ start=1500
 end=1500
 increment=500
 
-upload_base_dir=/gs/bs/tgh-NII-LLM/checkpoints/megatron-to-hf/Llama-3-8b-hf/LR1.0E-4-MINLR1.0E-5-WD0.1
+base_dirs=(
+  "/gs/bs/tgh-NII-LLM/checkpoints/Llama-3-8b/swallow-ja_8-en_1_code_1/LR1.0e-5-MINLR1.0E-6-WD0.05"
+  "/gs/bs/tgh-NII-LLM/checkpoints/Llama-3-8b/swallow-ja_8-en_1_code_1/LR2.5e-5-MINLR2.5E-6-WD0.05"
+)
 
-for ((i = start; i <= end; i += increment)); do
-  upload_dir=$upload_base_dir/iter_$(printf "%07d" $i)
+for base_dir in "${base_dirs[@]}"; do
+  for ((i = start; i <= end; i += increment)); do
+    upload_dir=$base_dir/iter_$(printf "%07d" $i)
 
-  python scripts/abci/upload/upload.py \
-    --ckpt-path $upload_dir \
-    --repo-name tokyotech-llm/Llama3-8b-LR1.0E-4-MINLR1.0E-5-WD0.1-iter$(printf "%07d" $i)
+    python scripts/abci/upload/upload.py \
+      --ckpt-path $upload_dir \
+      --repo-name tokyotech-llm/Llama3-8b-$(basename $base_dir)-iter$(printf "%07d" $i)
+  done
 done

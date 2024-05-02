c025761d179ff479416404c4eaae17505b489382
kazuki <kaz.tokyo.tech20@gmail.com>
2024-05-02 13:33:35 +0900

    fix: upload dir

--- scripts/tsubame/upload/upload_llama3_8b.sh
+++ scripts/tsubame/upload/upload_llama3_8b.sh
@@ -7,8 +7,8 @@ end=1500
 increment=500
 
 base_dirs=(
-  "/gs/bs/tgh-NII-LLM/checkpoints/Llama-3-8b/swallow-ja_8-en_1_code_1/LR1.0e-5-MINLR1.0E-6-WD0.05"
-  "/gs/bs/tgh-NII-LLM/checkpoints/Llama-3-8b/swallow-ja_8-en_1_code_1/LR2.5e-5-MINLR2.5E-6-WD0.05"
+  "/gs/bs/tgh-NII-LLM/checkpoints/megatron-to-hf/Llama-3-8b-hf/LR1.0e-5-MINLR1.0E-6-WD0.05"
+  "/gs/bs/tgh-NII-LLM/checkpoints/megatron-to-hf/Llama-3-8b-hf/LR2.5e-5-MINLR2.5E-6-WD0.05"
 )
 
 for base_dir in "${base_dirs[@]}"; do

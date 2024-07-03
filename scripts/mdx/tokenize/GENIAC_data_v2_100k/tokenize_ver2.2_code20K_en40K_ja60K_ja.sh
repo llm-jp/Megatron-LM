b7cb53ee66376c5edee8aa0138955b728b152e49
llmjp0 <is-llmjp@grp.tohoku.ac.jp>
2024-07-04 02:24:15 +0900

    tokenize_ver2.2_code20K_en40K_ja60K GENIAC_data

---
+++ scripts/mdx/tokenize/GENIAC_data_v2_100k/tokenize_ver2.2_code20K_en40K_ja60K_ja.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -e
+
+cd /model/llmjp0/Megatron-LM/
+source .env/bin/activate
+
+MODEL_PATH="/model/llmjp0/llm-jp-tokenizer/models/ver2.2/code20K_en40K_ja60K.ver2.2.model"
+OUTPUT_DIR="/data/llm-jp-corpus/v3.0.0/training_resharded_tokenize_ver2.2_code20K_en40K_ja60K.ver2.2/train/ja"
+OUTPUT_TOKEN_INFO="/model/llmjp0/Megatron-LM/scripts/mdx/tokenize/GENIAC_data_v2_100k/token_info/code20K_en40K_ja60K.ver2.2_ja.csv"
+mkdir -p $OUTPUT_DIR
+
+INPUT_DIR="/data/llm-jp-corpus/v3.0.0/training_resharded/train/ja/"
+
+for file in "$INPUT_DIR"/*.jsonl; 
+do
+  base_name=$(basename "$file")
+  output_path="$OUTPUT_DIR/$base_name"
+  python tools/preprocess_data.py \
+    --input "$file" \
+    --output-result-total-token-info $OUTPUT_TOKEN_INFO \
+    --output-prefix "$output_path" \
+    --tokenizer-model $MODEL_PATH \
+    --tokenizer-type Llama2Tokenizer \
+    --workers 256 \
+    --append-eod
+done

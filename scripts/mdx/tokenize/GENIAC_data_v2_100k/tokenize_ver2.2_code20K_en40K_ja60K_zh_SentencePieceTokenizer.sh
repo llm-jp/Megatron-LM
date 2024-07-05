5ee39f0bd2d9a8105f4c5005bd22fe7d39c0b504
llmjp0 <is-llmjp@grp.tohoku.ac.jp>
2024-07-05 17:34:17 +0900

    Script for tokenize_ver2.2_code20K_en40K_en60K SentencePieceTokenizer with GENIAC data

---
+++ scripts/mdx/tokenize/GENIAC_data_v2_100k/tokenize_ver2.2_code20K_en40K_ja60K_zh_SentencePieceTokenizer.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -e
+
+cd /model/llmjp0/Megatron-LM/
+source .env/bin/activate
+
+MODEL_PATH="/model/llmjp0/llm-jp-tokenizer/models/ver2.2/code20K_en40K_ja60K.ver2.2.model"
+OUTPUT_DIR="/data/llm-jp-corpus/v3.0.0/training_resharded_tokenize_ver2.2_code20K_en40K_ja60K.ver2.2_SentencePieceTokenizer/train/zh"
+OUTPUT_TOKEN_INFO="/model/llmjp0/Megatron-LM/scripts/mdx/tokenize/GENIAC_data_v2_100k/token_info_SentencePieceTokenizer/code20K_en40K_ja60K.ver2.2_zh.csv"
+mkdir -p $OUTPUT_DIR
+
+INPUT_DIR="/data/llm-jp-corpus/v3.0.0/training_resharded/train/zh/"
+
+for file in "$INPUT_DIR"/*.jsonl; 
+do
+  base_name=$(basename "$file")
+  output_path="$OUTPUT_DIR/$base_name"
+  python tools/preprocess_data.py \
+    --input "$file" \
+    --output-result-total-token-info $OUTPUT_TOKEN_INFO \
+    --output-prefix "$output_path" \
+    --tokenizer-model $MODEL_PATH \
+    --tokenizer-type SentencePieceTokenizer \
+    --workers 256 \
+    --append-eod
+done

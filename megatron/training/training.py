11580059118ec67f31b92bd08d5e9a5011d6e484
Taishi-N324 <taishiahiru5@gmail.com>
2025-10-24 16:57:55 +0900

    Add minimal logging

--- megatron/training/training.py
+++ megatron/training/training.py
@@ -1471,6 +1471,8 @@ def training_log(
                 "mem-max-allocated-bytes", mem_stats["allocated_bytes.all.peak"], iteration
             )
             writer.add_scalar("mem-allocated-count", mem_stats["allocation.all.current"], iteration)
+    import typing
+    wandb_stats: dict[str, typing.Any] = {}
     if args.num_experts is not None:
         moe_loss_scale = 1 / get_num_microbatches()
         track_names = []
@@ -1480,7 +1482,7 @@ def training_log(
             track_names.append("seq_load_balancing_loss")
         if args.moe_z_loss_coeff is not None:
             track_names.append("z_loss")
-        track_moe_metrics(
+        wandb_stats = track_moe_metrics(
             loss_scale=moe_loss_scale,
             iteration=iteration,
             writer=writer,
@@ -1492,12 +1494,45 @@ def training_log(
             num_layers=args.num_layers,
             moe_layer_freq=args.moe_layer_freq,
             mtp_num_layers=args.mtp_num_layers,
+            wandb_stats=wandb_stats,
         )
     if args.mtp_num_layers is not None:
         mtp_loss_scale = 1 / get_num_microbatches()
         MTPLossLoggingHelper.track_mtp_metrics(
             mtp_loss_scale, iteration, writer, wandb_writer, total_loss_dict
         )
+    if wandb_writer and (iteration % args.tensorboard_log_interval == 0) and is_last_rank():
+        wandb_stats["utils/steps-vs-samples"] = args.consumed_train_samples
+
+        wandb_stats["utils/learning-rate"] = learning_rate
+        wandb_stats["utils/batch-size"] = batch_size
+
+        for key in loss_dict:
+            wandb_stats[f"lm-loss-training/{key}"] = loss_dict[key]
+            wandb_stats[f"lm-loss-training/{key}_ppl"] = math.exp(total_loss_dict[key].item())
+
+        wandb_stats["others/loss-scale"] = loss_scale
+        wandb_stats["others/grad-norm"] = grad_norm
+        if hasattr(args, 'seq_length'):
+            wandb_stats["others/seq_length"] = args.seq_length
+        if hasattr(args, 'num_experts'):
+            wandb_stats["others/num_experts"] = args.num_experts
+        if hasattr(args, 'moe_router_topk'):
+            wandb_stats["others/moe_router_topk"] = args.moe_router_topk
+
+        if optimizer is not None:
+            wandb_stats['optimizer/variance_l2'] = opt_stats[0]**0.5
+            wandb_stats['optimizer/variance_sqrt_l2'] = opt_stats[1]**0.5
+            wandb_stats['optimizer/momentum_l2'] = opt_stats[2]**0.5
+            wandb_stats['optimizer/weight_l2'] = opt_stats[3]**0.5
+            wandb_stats['optimizer/variance_l1'] = opt_stats[4]
+            wandb_stats['optimizer/variance_sqrt_l1'] = opt_stats[5]
+            wandb_stats['optimizer/momentum_l1'] = opt_stats[6]
+            wandb_stats['optimizer/weight_l1'] = opt_stats[7]
+            wandb_stats['optimizer/variance_abs_max'] = opt_stats_2[0]
+            wandb_stats['optimizer/variance_sqrt_abs_max'] = opt_stats_2[1]
+            wandb_stats['optimizer/momentum_abs_max'] = opt_stats_2[2]
+            wandb_stats['optimizer/weight_abs_max'] = opt_stats_2[3]
     if iteration % args.log_interval == 0:
         if args.record_memory_history and is_last_rank():
             snapshot = torch.cuda.memory._snapshot()
@@ -1514,6 +1549,14 @@ def training_log(
         )
 
         one_logger_utils.track_e2e_metrics(args.log_throughput, throughput)
+        tokens_per_sec = args.global_batch_size *  args.seq_length / elapsed_time_per_iteration
+        wandb_stats["stats/tflops"] = throughput
+        wandb_stats["stats/1_iteration_time"] = elapsed_time_per_iteration
+        wandb_stats["stats/tokens_per_sec"] = tokens_per_sec
+        wandb_stats["stats/tokens_per_sec_per_gpu"] = tokens_per_sec / args.world_size
+
+        if wandb_writer and is_last_rank():
+            wandb_writer.log(wandb_stats, step=iteration)
 
         if args.log_timers_to_tensorboard:
             if writer:
